3b7f08032a7df1508541d4826e3db1f776cf16a6
harry <harry@rooit.me>
2021-11-08 18:56:03 +0800

    first commit

---
+++ PlayVideoInChatRoom/VideoView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoView.swift
+//  PlayVideoInChatRoom
+//
+//  Created by Harry on 2021/11/8.
+//
+
+import UIKit
+
+class VideoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
